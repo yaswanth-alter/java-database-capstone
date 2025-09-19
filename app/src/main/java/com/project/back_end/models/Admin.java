6fcb0e2f6578fb6da5b6a7c71dc1265997a43689
Dev085 <diana-ev@hotmail.com>
2025-09-19 16:42:10 -0500

    Update Admin.java

--- app/src/main/java/com/project/back_end/models/Admin.java
+++ app/src/main/java/com/project/back_end/models/Admin.java
@@ -36,7 +36,7 @@
 
 //}
 
-package com.project.back_end.models;
+/*package com.project.back_end.models;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.*;
@@ -69,5 +69,51 @@ public class Admin {
     }
 
     // Getters and setters
+}*/
+
+package com.project.back_end.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@Entity
+public class Admin {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull(message = "username cannot be null")
+    private String username;
+
+    @NotNull(message = "password cannot be null")
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String password;
+
+    // Getters and setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    // No getter for password to keep it write-only in JSON
 }
+
 
